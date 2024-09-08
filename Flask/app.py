53952455884775f0ce6eccc860bb287a9ae4b286
GhostriderDK <149684275+GhostriderDK@users.noreply.github.com>
2024-09-08 23:08:21 +0200

    html fun

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,5 @@
 import base64
 from io import BytesIO
-import matplotlib.pyplot as plt
 from flask import Flask, render_template
 from matplotlib.figure import Figure
 from get_data import *
@@ -11,35 +10,35 @@ app.run(debug=True)
 datapoints = 1000
 
 def neon_data():
-    temp, soil, info = get_neon_pothos_data(datapoints)
+    timestamps, temp, soil, info = get_neon_pothos_data(datapoints)
+   
+    fig = Figure() 
+    ax = fig.add_subplot(1, 1)
+    fig.subplots_adjust(bottom=0.1)
+    ax.set_facecolor("white")
+    ax.plot(timestamps, soil, linestyle="solid", c="#11f", linewidth="1.5")
+    ax.set_ylabel("moisture in %")
+    ax.tick_params(axis="y", colors="blue")
+    ax.spines["left"].set_color("blue")
+    tick_positions = range(0, len(timestamps), len(timestamps) // num_ticks)
+    ax.set_xticks(tick_positions)
+    ax.set_xticklabels([])
+    ax.grid(axis='y', linestyle='--')
+    ax.set_title("Neon Pothos Moisture Level", fontsize=15)
+    
+    fig.subplots_adjust(bottom=0.3)
+    fig.patch.set_facecolor("orange")
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return data
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
 
 @app.route('/moisture-graph')
 def moisture_graph():
-    # Generate some sample moisture data
-    days = np.arange(1, 11)
-    moisture_levels = np.random.randint(0, 100, size=10)
-
-    # Create the graph using Figure module
-    fig = Figure()
-    ax = fig.add_subplot(111)
-    ax.plot(days, moisture_levels)
-    ax.set_xlabel('Day')
-    ax.set_ylabel('Moisture Level')
-    ax.set_title('Moisture Data')
-    
-    # Save the graph to a BytesIO object
-    buffer = BytesIO()
-    fig.savefig(buffer, format='png')
-    buffer.seek(0)
-
-    # Encode the graph image as base64
-    # image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
-
-    # Render the template with the graph image
-    
-    return render_template('moisture_graph.html')
+    neonpothos_data = neon_data()
+    return render_template('moisture_graph.html', neonpothos_data=neonpothos_data)
     # return render_template('moisture_graph.html', image_base64=image_base64)

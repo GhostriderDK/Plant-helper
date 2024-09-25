adc9b4fe261875929d4cef398cd4ab6ab20fd487
GhostriderDK <149684275+GhostriderDK@users.noreply.github.com>
2024-09-25 19:38:56 +0200

    Refactor Flask app.py and backend.py for improved error handling and exception logging

--- Flask/app.py
+++ Flask/app.py
@@ -17,71 +17,97 @@ def home():
 
 @app.route('/overview')
 def overview():
-    global last_log
+    try:
+        global last_log
 
-    peperomia_level = bk.get_peperomia_level()
-    if peperomia_level == "Wet":
+        peperomia_level = bk.get_peperomia_level()
+        if peperomia_level == "Wet":
+            peperomia_class = "wet"
+        elif peperomia_level == "Moist":
+            peperomia_class = "moist"
+        elif peperomia_level == "Dry":
+            peperomia_class = "dry"
+        
+        avg_peperomia = bk.get_current_pct_peperomia()
+        
+        watering_timestamp = bk.peperomia_watering()
+        if watering_timestamp != None:
+            last_water_peperomia = watering_timestamp
+            last_log = watering_timestamp
+        else:
+            last_water_peperomia = last_log
+            
+        neonpothos_level = bk.get_neonpothos_level()
+        if neonpothos_level == "Wet":
+            neonpothos_class = "wet"
+        elif neonpothos_level == "Moist":
+            neonpothos_class = "moist"
+        elif neonpothos_level == "Dry":
+            neonpothos_class = "dry"
+    except Exception as ex:
+        print(ex)
+        peperomia_level = "N/A"
         peperomia_class = "wet"
-    elif peperomia_level == "Moist":
-        peperomia_class = "moist"
-    elif peperomia_level == "Dry":
-        peperomia_class = "dry"
-    
-    avg_peperomia = bk.get_current_pct_peperomia()
-    
-    watering_timestamp = bk.peperomia_watering()
-    if watering_timestamp != None:
-        last_water_peperomia = watering_timestamp
-        last_log = watering_timestamp
-    else:
-        last_water_peperomia = last_log
-          
-    neonpothos_level = bk.get_neonpothos_level()
-    if neonpothos_level == "Wet":
-        neonpothos_class = "wet"
-    elif neonpothos_level == "Moist":
-        neonpothos_class = "moist"
-    elif neonpothos_level == "Dry":
-        neonpothos_class = "dry"
-    return render_template('overview.html', peperomia_level=peperomia_level, neonpothos_level=neonpothos_level,
+        avg_peperomia = "N/A"
+        last_water_peperomia = "N/A"
+        neonpothos_level = "N/A"
+        neonpothos_class = "    
+    finally:
+        return render_template('overview.html', peperomia_level=peperomia_level, neonpothos_level=neonpothos_level,
                             peperomia_class=peperomia_class, neonpothos_class=neonpothos_class, avg_peperomia=avg_peperomia, last_water_peperomia=last_water_peperomia)
 
 @app.route('/moisture-graph')
 def moisture_graph():
-    peperomia_graph = bk.peperomia_data(datapoints, num_ticks)
-    return render_template('moisture_graph.html', peperomia_graph=peperomia_graph)
+    try:
+        peperomia_graph = bk.peperomia_data(datapoints, num_ticks)
+    except Exception as ex:
+        print(ex)
+        peperomia_graph = None
+    finally:    
+        return render_template('moisture_graph.html', peperomia_graph=peperomia_graph)
     # return render_template('moisture_graph.html', image_base64=image_base64)
 
 @app.route('/overview-graph')
 def overview_graph():
-    peperomia_graph = bk.peperomia_data(datapoints, num_ticks)
-    global last_log
+    try:
+        peperomia_graph = bk.peperomia_data(datapoints, num_ticks)
+        global last_log
 
-    peperomia_level = bk.get_peperomia_level()
-    if peperomia_level == "Wet":
+        peperomia_level = bk.get_peperomia_level()
+        if peperomia_level == "Wet":
+            peperomia_class = "wet"
+        elif peperomia_level == "Moist":
+            peperomia_class = "moist"
+        elif peperomia_level == "Dry":
+            peperomia_class = "dry"
+        
+        avg_peperomia = bk.get_current_pct_peperomia()
+        
+        watering_timestamp = bk.peperomia_watering()
+        if watering_timestamp != None:
+            last_water_peperomia = watering_timestamp
+            last_log = watering_timestamp
+        else:
+            last_water_peperomia = last_log
+            
+        neonpothos_level = bk.get_neonpothos_level()
+        if neonpothos_level == "Wet":
+            neonpothos_class = "wet"
+        elif neonpothos_level == "Moist":
+            neonpothos_class = "moist"
+        elif neonpothos_level == "Dry":
+            neonpothos_class = "dry"
+    except Exception as ex:
+        print(ex)
+        peperomia_graph = None
+        peperomia_level = "N/A"
         peperomia_class = "wet"
-    elif peperomia_level == "Moist":
-        peperomia_class = "moist"
-    elif peperomia_level == "Dry":
-        peperomia_class = "dry"
-    
-    avg_peperomia = bk.get_current_pct_peperomia()
-    
-    watering_timestamp = bk.peperomia_watering()
-    if watering_timestamp != None:
-        last_water_peperomia = watering_timestamp
-        last_log = watering_timestamp
-    else:
-        last_water_peperomia = last_log
-          
-    neonpothos_level = bk.get_neonpothos_level()
-    if neonpothos_level == "Wet":
+        avg_peperomia = "N/A"
+        last_water_peperomia = "N/A"
+        neonpothos_level = "N/A"
         neonpothos_class = "wet"
-    elif neonpothos_level == "Moist":
-        neonpothos_class = "moist"
-    elif neonpothos_level == "Dry":
-        neonpothos_class = "dry"
-    return render_template('overview-graph.html', peperomia_graph=peperomia_graph, peperomia_level=peperomia_level, neonpothos_level=neonpothos_level,
+    finally:
+        return render_template('overview-graph.html', peperomia_graph=peperomia_graph, peperomia_level=peperomia_level, neonpothos_level=neonpothos_level,
                             peperomia_class=peperomia_class, neonpothos_class=neonpothos_class, avg_peperomia=avg_peperomia, last_water_peperomia=last_water_peperomia)
 
 @app.route('/plant-bot')
--- Flask/backend.py
+++ Flask/backend.py
@@ -4,65 +4,85 @@ from matplotlib.figure import Figure
 from get_data import *
 
 def peperomia_data(datapoints, num_ticks):
-    timestamps, soil, time, info = get_peperomia_data(datapoints)
-   
-    fig = Figure()
-    ax = fig.subplots()
-    fig.subplots_adjust(bottom=0.3)
+    try:
+        timestamps, soil, time, info = get_peperomia_data(datapoints)
     
-    ax.tick_params(axis='x', which='both', rotation=90)
-    ax.set_facecolor("white")
-    ax.plot(timestamps, soil, linestyle="solid", c="#11f", linewidth="1.5")
-    ax.set_xlabel("Timestamps")
-    ax.set_ylabel("moisture in %")
-    ax.tick_params(axis="x", colors="black")
-    ax.tick_params(axis="y", colors="blue")
-    ax.spines["left"].set_color("blue")
-    ax.grid(axis='y', linestyle='--')
-    num_ticks = min(num_ticks, len(timestamps))
-    tick_positions = range(0, len(timestamps), max(1, len(timestamps) // num_ticks))
-    ax.set_xticks(tick_positions)
-    fig.patch.set_facecolor("white")
-    
-    buf = BytesIO()
-    fig.savefig(buf, format="png")
-    data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    return data
+        fig = Figure()
+        ax = fig.subplots()
+        fig.subplots_adjust(bottom=0.3)
+        
+        ax.tick_params(axis='x', which='both', rotation=90)
+        ax.set_facecolor("white")
+        ax.plot(timestamps, soil, linestyle="solid", c="#11f", linewidth="1.5")
+        ax.set_xlabel("Timestamps")
+        ax.set_ylabel("moisture in %")
+        ax.tick_params(axis="x", colors="black")
+        ax.tick_params(axis="y", colors="blue")
+        ax.spines["left"].set_color("blue")
+        ax.grid(axis='y', linestyle='--')
+        num_ticks = min(num_ticks, len(timestamps))
+        tick_positions = range(0, len(timestamps), max(1, len(timestamps) // num_ticks))
+        ax.set_xticks(tick_positions)
+        fig.patch.set_facecolor("white")
+        
+        buf = BytesIO()
+        fig.savefig(buf, format="png")
+        data = base64.b64encode(buf.getbuffer()).decode("ascii")
+        return data
+    except Exception as ex:
+        print(ex)
+        return None
 
 def get_peperomia_level():
-    timestamps, soil, time, info = get_peperomia_data(1)
+    try:
+        timestamps, soil, time, info = get_peperomia_data(1)
+        
+        if info.count(info[0]) == len(info):
+            return info[0]
+        else:
+            pass
+    except Exception as ex:
+        print(ex)
+        return "N/A"
     
-    if info.count(info[0]) == len(info):
-        return info[0]
-    else:
-        pass
 
 def get_current_pct_peperomia():
-    timestamps, soil, time, inf = get_peperomia_data(3)
-    avg_soil = sum(soil) / len(soil)
-    
-    avg_soil = round(avg_soil, 0)
-    
-    return avg_soil 
+    try:
+        timestamps, soil, time, inf = get_peperomia_data(3)
+        avg_soil = sum(soil) / len(soil)
+        
+        avg_soil = round(avg_soil, 0)
+        
+        return avg_soil
+    except Exception as ex:
+        print(ex)
+        return "N/A" 
 
 def peperomia_watering(data_points=1000):
-    timestamps, soil, time, info = get_peperomia_data(data_points)
-    increase_threshold = 10  # Define the threshold for increase
-    last_watering_timestamp = None  # Initialize to store the last detected watering timestamp
+    try:
+        timestamps, soil, time, info = get_peperomia_data(data_points)
+        increase_threshold = 10  # Define the threshold for increase
+        last_watering_timestamp = None  # Initialize to store the last detected watering timestamp
 
-    for i in range(1, len(soil)):
-        if soil[i] - soil[i - 1] > increase_threshold:
-            last_watering_timestamp = timestamps[i]
+        for i in range(1, len(soil)):
+            if soil[i] - soil[i - 1] > increase_threshold:
+                last_watering_timestamp = timestamps[i]
 
-    return last_watering_timestamp
-    
+        return last_watering_timestamp
+    except Exception as ex:
+        print(ex)
+        return "N/A"
     
     
 
 def get_neonpothos_level(timestamps="", soil = 50, time = 1, info = ["Moist", "Moist"]):
-    timestamps, soil, time, inf = get_neon_pothos_data(10)
-    
-    if info.count(info[0]) == len(info):
-        return info[0]
-    else:
-        pass
+    try:
+        timestamps, soil, time, inf = get_neon_pothos_data(10)
+        
+        if info.count(info[0]) == len(info):
+            return info[0]
+        else:
+            pass
+    except Exception as ex:
+        print(ex)
+        return "N/A"
